259092c7eb5f9563bf7be9096a7c9bb0556e870c
Farzan <farzan@OFD001M6014.local>
2025-11-10 00:49:11 +0400

    work on func

--- basic/function/def.py
+++ basic/function/def.py
@@ -1,8 +1,15 @@
 # say_hello() # this will give an error. there is no hoisting here!
 
-def say_hello():
-    print('my hello')
+# with default parameter
+def say_hello(name='jedi', emoji='=)'): # parameter
+    print(f'my hello {name}, {emoji}')
+
+print(say_hello) # show the place of the function in the memory
 
 say_hello()
 
-print(say_hello) # show the place of the function in the memory
+# positional arguments (matters where we put the arg)
+say_hello('fffarzan', ':)') 
+
+# keyowrd arguments
+say_hello(emoji=':)', name='fffarzan')
