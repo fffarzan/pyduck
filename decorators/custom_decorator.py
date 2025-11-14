e833565616fafe16fc7baede4334384b818c364c
Farzan <farzan@OFD001M6014.local>
2025-11-14 18:25:25 +0400

    finish decorators

---
+++ decorators/custom_decorator.py
@@ -0,0 +1,31 @@
+# A decorator super charges a function. It wraps another function and enchances it.
+
+def my_dec(func):
+    def wrap_func():
+        print('***************')
+        func()
+    
+    return wrap_func
+
+@my_dec
+def hello():
+    print('hellooo')
+
+hello()
+
+# decorator is like of these calls:
+my_dec(hello())
+my_dec(hello)()
+
+def my_other_dec(func):
+    def wrap_func(*args, **kwargs):
+        print('***************')
+        func(*args, **kwargs)
+
+    return wrap_func
+
+@my_other_dec
+def my_other_hello(greeting, emoji=':)'):
+    print(greeting, emoji)
+
+my_other_hello('Hiiii')
